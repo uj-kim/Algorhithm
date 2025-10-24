9ffbf1f234baef18c154ae2b0ae327bd2ba50d34
kimmikmikimmim <67899735+uj-kim@users.noreply.github.com>
2025-10-24 22:43:21 +0900

    [Silver III] Title: 01타일, Time: 236 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+if n == 1:
+  print(1)
+elif n == 2:
+  print(2)
+else:
+  a, b = 1, 2
+  for _ in range(3, n + 1):
+    a, b = b, (a+b) % 15746
+  print(b)
